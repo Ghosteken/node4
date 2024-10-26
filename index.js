f4b2f5c33a22a3eb4d8392cb41e57998a8a3d975
nicholas <aigberuan6@gmail.com>
2024-10-26 06:13:06 +0100

    api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require("express")
+const express = require("express");
+const { Mongoose, default: mongoose } = require("mongoose");
 const app = express()
 PORT = 8000
 
@@ -7,8 +8,43 @@ app.get('/', (req, res) => {
     res.send("good boy")
 });
 
+
+
 //if you want to implement this accross all the end points
 app.use(middleware);
+// middleware to read request data in post and put and convert it to js object
+app.use(express.json())
+
+//database connection
+//promise handling
+mongoose.connect('mongodb://localhost:2017/mogo1')
+.then(()=>{
+    console.log('connection sucsessfull')
+}).catch((err)=>{
+    console.log(err)
+})
+
+const productschma = mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'Name is a must']
+    },
+    price:{
+        type:Number,
+        required:[true,'Price is a must'],
+        min: 1
+    },
+    Quantity:{
+        type:Number,
+        required:[true,'!!!'],
+    },
+    category:{
+        type:String,
+        enum:['clothing','electronics','household']
+    }
+},{timestaps:true})
+
+const productModel = mongoose.model('products',productschma)
 
 // you can pass muliple vars
 app.get('/users/:id/:name',(req,res)=>{
@@ -19,6 +55,76 @@ app.get('/users/:id/:name',(req,res)=>{
 app.get('/testing/:id',middleware,(req,res)=>{
     console.log("main endpoint")
     res.send({message:'testing request'})
+})
+
+app.get('/products/:id',(req,res)=>{
+    productModel.findOne({_id:req.params.id})
+    .then((product)=>{
+        res.send(product)
+        
+    }).catch((err)=>{
+        console.log(err)
+        res.send("problem")
+    })
+})
+
+
+app.get('/products',(req,res)=>{
+    productModel.find()
+    .then((products)=>{
+        res.send(products)
+    }).catch((err)=>{
+        console.log(err)
+        res.send("problem")
+    })
+})
+
+app.delete('/product/:id',(req,res)=>{
+    productModel.deleteOne({_id:req.params.id})
+    .then(()=>{
+        res.send({message:'product deleted'})
+        
+    }).catch((err)=>{
+        console.log(err)
+        res.send("problem")
+    })
+})
+
+app.put('/products/:id',(req,res)=>{
+    product = req.body
+    productModel.updateOne({_id:req.params.id},product)
+    .then(()=>{
+        res.send({message:'product updated'})
+        console.log("product updated")
+    }).catch((err)=>{
+        console.log(err)
+        res.send('updated')
+    })
+})
+
+//former aproach
+app.post('products',(req,res)=>{
+
+    const product = req.body
+    productModel.create(product)
+    .then((document)=>{
+        res.send({document,message:'product added'})
+        console.log('product added')
+    }).catch((err)=>{
+        res.send({message:'operation failed'})
+        console.log(err)
+    })
+    console.log(req.body)
+    //under the hood
+    // let product = '';
+    // req.on('data',(chunk)=>{
+    //     product +=chunk;
+    // })
+
+    // req.on('end',()=>{
+    //     console.log(JSON.parse(product))
+    // })
+
 })
 
 function middleware(req,res,next){
--- note.js
+++ note.js
@@ -1,13 +1,14 @@
-const express = require('express')
+// const express = require('express')
 
-const app = express();
-port = 8000
+// const app = express();
+// port = 8000
 
-app.get('/back',(req,res)=>{
-    res.send('we are togeter a gain')
-})
+// app.get('/back',(req,res)=>{
+//     res.send('we are togeter a gain')
+// })
 
 
-app.listen(port,(req,res)=>{
-    console.log("server up and running")
-})
+// app.listen(port,(req,res)=>{
+//     console.log("server up and running")
+// })
+
